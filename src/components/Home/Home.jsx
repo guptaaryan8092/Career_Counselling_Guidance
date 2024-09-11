6d0bb5f1a8544a230944c168248b7ec345f18c07
guptaaryan8092 <guptaaryan8092@gmail.com>
2024-09-11 21:45:22 +0530

    path change

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div>
     <img
-        src="src\assets\img\Bodyimage\bodyimage.gif"
+        src="/assets/img/Bodyimage/bodyimage.gif"
         alt="Description of GIF"
         style={{ width: '100%', height: 'auto' }} // Adjust these values as needed
     />
