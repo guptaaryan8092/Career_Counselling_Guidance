624f2dda64fd8a371c92c21408b468cc186e2338
guptaaryan8092 <guptaaryan8092@gmail.com>
2024-09-13 03:00:03 +0530

    footer a tag to link tag convert

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -18,10 +18,10 @@ function Footer() {
                   <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-lime-700">Resources</h2>
                   <ul class="text-gray-500 dark:text-gray-400 font-medium">
                       <li class="mb-4">
-                          <a href="/test" class="hover:underline">Test</a>
+                          <link href="/test" class="hover:underline">Test</link>
                       </li>
                       <li>
-                          <a href="/roadmap" class="hover:underline">Roadmap</a>
+                          <link href="/roadmap" class="hover:underline">Roadmap</link>
                       </li>
                   </ul>
               </div>
